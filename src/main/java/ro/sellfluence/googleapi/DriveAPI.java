4c05e481a4d07de2d4aafae809b0d8cd631f9263
Claudio Nieder <claudio.nieder@inodes.ch>
2024-03-16 00:41:56 +0100

    Implemented getId method.

---
+++ src/main/java/ro/sellfluence/googleapi/DriveAPI.java
@@ -0,0 +1,63 @@
+package ro.sellfluence.googleapi;
+
+import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
+import com.google.api.client.json.JsonFactory;
+import com.google.api.client.json.gson.GsonFactory;
+import com.google.api.services.drive.Drive;
+import com.google.api.services.drive.model.File;
+
+import java.io.IOException;
+import java.security.GeneralSecurityException;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
+import static com.google.api.services.drive.DriveScopes.DRIVE_METADATA_READONLY;
+import static com.google.api.services.drive.DriveScopes.DRIVE_READONLY;
+import static ro.sellfluence.googleapi.Credentials.getCredentials;
+
+public class DriveAPI {
+    private static final JsonFactory jsonFactory = GsonFactory.getDefaultInstance();
+    private static final List<String> scopes = List.of(DRIVE_READONLY, DRIVE_METADATA_READONLY);
+    private final String appName;
+
+    /**
+     * Initialize the drive API for an app.
+     * @param appName name of the app as registered in the <a href="https://console.cloud.google.com/apis/credentials/consent">console</a>
+     */
+    public DriveAPI(String appName) {
+        this.appName = appName;
+    }
+
+    /**
+     * Find the d of a file given its name.
+     *
+     * @param name of the file.
+     * @return the id of the file.
+     * @throws Exception if something goes wrong.
+     */
+    public String getFileId(String name) throws Exception {
+        Objects.requireNonNull(name);
+        var matchingFiles = setupDriveService().files().list().execute().getFiles().stream()
+                .filter(f -> name.equals(f.getName()))
+                .map(File::getId)
+                .collect(Collectors.toSet());
+        if (matchingFiles.isEmpty()) {
+            return null;
+        } else if (matchingFiles.size() == 1) {
+            return matchingFiles.iterator().next();
+        } else {
+            throw new RuntimeException("More than one file with matches %s.".formatted(name));
+        }
+    }
+
+    private Drive setupDriveService() throws GeneralSecurityException, IOException {
+        return new Drive.Builder(
+                GoogleNetHttpTransport.newTrustedTransport(),
+                jsonFactory,
+                getCredentials(GoogleNetHttpTransport.newTrustedTransport(), scopes)
+        )
+                .setApplicationName(appName)
+                .build();
+    }
+}
