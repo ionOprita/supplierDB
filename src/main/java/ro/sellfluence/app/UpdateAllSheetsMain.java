28ccd5ba652449324665645f9404c0d3a54d459d
Claudio Nieder <claudio.nieder@inodes.ch>
2024-10-04 15:40:12 +0200

    Better error message when token gets invalid and is not refreshed.

--- src/main/java/ro/sellfluence/app/UpdateAllSheetsMain.java
+++ src/main/java/ro/sellfluence/app/UpdateAllSheetsMain.java
@@ -1,19 +1,30 @@
 package ro.sellfluence.app;
 
+import com.google.api.client.auth.oauth2.TokenResponseException;
 import ro.sellfluence.emagapi.EmagApi;
 
 import java.util.logging.Logger;
 
 import static java.util.logging.Level.WARNING;
+import static ro.sellfluence.googleapi.Credentials.tokenStorePath;
 
 public class UpdateAllSheetsMain {
     public static void main(String[] args) {
         Logger.getLogger(EmagApi.class.getName()).setLevel(WARNING);
-        new TransferFromEmagToSheets(
-                "sellfluence1",
-                "Testing Coding 2024 - Date Produse & Angajati",
-                "Cons. Date Prod."
-        )
-                .transferFromEmagToSheet("emag", "emag2");
+        try {
+            new TransferFromEmagToSheets(
+                    "sellfluence1",
+                    "Testing Coding 2024 - Date Produse & Angajati",
+                    "Cons. Date Prod."
+            )
+                    .transferFromEmagToSheet("emag", "emag2");
+        } catch (Exception e) {
+            if (e instanceof TokenResponseException) {
+                System.out.println("Try to delete " + tokenStorePath);
+            } else {
+                System.out.println("Maybe you can try to delete " + tokenStorePath);
+                throw e;
+            }
+        }
     }
 }
