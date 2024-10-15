733e7376e94f7ebc4ff1cda42f913770c56af7b2
Claudio Nieder <claudio.nieder@inodes.ch>
2024-10-15 13:44:31 +0200

    Adapted to fill a whole year, week by week.

--- src/main/java/ro/sellfluence/app/EmagDBApp.java
+++ src/main/java/ro/sellfluence/app/EmagDBApp.java
@@ -7,6 +7,7 @@ import ro.sellfluence.support.UserPassword;
 
 import java.io.IOException;
 import java.sql.SQLException;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Map;
@@ -14,32 +15,50 @@ import java.util.logging.Handler;
 import java.util.logging.Logger;
 
 import static java.util.logging.Level.FINE;
+import static java.util.logging.Level.INFO;
 import static java.util.logging.Level.SEVERE;
 import static java.util.logging.Level.WARNING;
-import static ro.sellfluence.emagapi.EmagApi.statusFinalized;
 
 public class EmagDBApp {
 
     private static final Logger logger = Logger.getLogger(EmagDBApp.class.getName());
+    private static final List<String> emagAccounts = List.of(
+            "sellfluence",
+            "sellfusion"
+//            "zoopieconcept",
+//            "zoopieinvest",
+//            "zoopiesolutions",
+//            "judios",
+//            "koppel",
+//            "koppelfbe"
+    );
 
     public static void main(String[] args) throws Exception {
         activateEmagJSONLog();
-        var mirrorDB = EmagMirrorDB.getEmagMirrorDB("emagOprita");
-        // ("sellfluence", "sellfusion", "zoopieconcept", "zoopieinvest", "zoopiesolutions", "judios", "koppel", "koppelfbe");
-        var accounts = List.of("sellfluence", "sellfusion", "zoopieconcept", "zoopieinvest", "zoopiesolutions", "judios", "koppel", "koppelfbe");
-        for (String account : accounts) {
-            var orders = readFromEmag(account, LocalDateTime.now().minusDays(1), LocalDateTime.now().minusDays(0));
-            if (orders != null) {
-                orders.forEach(orderResult ->
-                        {
-                            try {
-                                mirrorDB.addOrder(orderResult);
-                            } catch (SQLException e) {
-                                logger.log(SEVERE, "Error inserting order " + orderResult, e);
-                            }
-                        }
-                );
+        var mirrorDB = EmagMirrorDB.getEmagMirrorDB("emagLocal");
+        var startOfToday = LocalDate.now().atStartOfDay();
+        for (int pastWeek=0; pastWeek<52; pastWeek++) {
+            var endTime = startOfToday.minusWeeks(pastWeek);
+            var startTime = endTime.minusWeeks(1);
+            for (String account : emagAccounts) {
+                logger.log(INFO, "Fetch from %s for %s - %s".formatted(account, startTime, endTime));
+                transferOrdersToDatabase(account, mirrorDB, startTime, endTime);
             }
+        }
+    }
+
+    private static void transferOrdersToDatabase(String account, EmagMirrorDB mirrorDB, LocalDateTime startTime, LocalDateTime endTime) throws IOException, InterruptedException {
+        var orders = readFromEmag(account, startTime, endTime);
+        if (orders != null) {
+            orders.forEach(orderResult ->
+                    {
+                        try {
+                            mirrorDB.addOrder(orderResult);
+                        } catch (SQLException e) {
+                            logger.log(SEVERE, "Error inserting order " + orderResult, e);
+                        }
+                    }
+            );
         }
     }
 
