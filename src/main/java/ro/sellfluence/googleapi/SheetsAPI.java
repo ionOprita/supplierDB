920c1f427864aeb1b890a255e7e90016cc023528
Claudio Nieder <claudio.nieder@inodes.ch>
2024-10-18 13:56:02 +0200

    Add explaining text to exception.

--- src/main/java/ro/sellfluence/googleapi/SheetsAPI.java
+++ src/main/java/ro/sellfluence/googleapi/SheetsAPI.java
@@ -189,7 +189,7 @@ public class SheetsAPI {
                 return null;
             }
         } catch (IOException e) {
-            throw new RuntimeException(e);
+            throw new RuntimeException("Error when reading the columns %s from the sheet %s.".formatted(columns, sheetName), e);
         }
     }
 
