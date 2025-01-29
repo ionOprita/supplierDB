7046b0cd78ddacf54ca3e16d03113edbca2208d3
Claudio Nieder <claudio.nieder@inodes.ch>
2025-01-30 00:53:51 +0100

    Dropped cancellatio_reason table in favour of an additional entry directly in emag_order.

--- src/main/java/ro/sellfluence/db/EmagMirrorDBVersion3.java
+++ src/main/java/ro/sellfluence/db/EmagMirrorDBVersion3.java
@@ -13,29 +13,10 @@ class EmagMirrorDBVersion3 {
      * @throws SQLException all errors are passed back to caller.
      */
     static void version3(Connection db) throws SQLException {
-        createCancellationReasonTable(db);
-        alterEmagOrderTable(db);
-    }
-
-    private static void createCancellationReasonTable(Connection db) throws SQLException {
-        executeStatement(db, """
-                CREATE TABLE cancellation_reason (
-                    id INTEGER,
-                    name VARCHAR(255),
-                    PRIMARY KEY (id),
-                );
-                """);
-    }
-    private static void alterEmagOrderTable(Connection db) throws SQLException {
-        executeStatement(db, """
+        try (var s = db.prepareStatement("""
                 ALTER TABLE emag_order
-                  ADD FOREIGN KEY (cancellation_reason)
-                  REFERENCES cancellation_reason (id);
-                """);
-    }
-
-    private static void executeStatement(Connection db, String createStatement) throws SQLException {
-        try (var s = db.prepareStatement(createStatement)) {
+                    ADD COLUMN cancellation_reason_text VARCHAR(255);
+                """)) {
             s.execute();
         }
     }
