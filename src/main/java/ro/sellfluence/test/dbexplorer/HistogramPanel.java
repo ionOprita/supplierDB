634e5f78ae474014bf44a6038b946e21234fdb20
Claudio Nieder <claudio.nieder@inodes.ch>
2025-02-11 21:45:02 +0100

    Added y Axis.

--- src/main/java/ro/sellfluence/test/dbexplorer/HistogramPanel.java
+++ src/main/java/ro/sellfluence/test/dbexplorer/HistogramPanel.java
@@ -34,16 +34,27 @@ public class HistogramPanel extends JPanel {
         int panelWidth = getWidth();
         int panelHeight = getHeight();
         int maxCount = histogram.stream().mapToInt(EmagFetchHistogram::count).max().orElse(1); // Find max count for scaling
-        int barWidth = panelWidth / histogram.size();
+        int heightOfBottomAxis = -30;
+        int widthOfLeftAxis = 60;
+        int barWidth = (panelWidth - widthOfLeftAxis) / histogram.size();
         for (int i = 0; i < histogram.size(); i++) {
             EmagFetchHistogram item = histogram.get(i);
             g.setColor(Color.ORANGE);
-            int barHeight = (int) ((double) item.count() / maxCount * panelHeight * 0.8); // Scale to 80% of panel height
-            int x = i * barWidth;
-            int y = panelHeight - barHeight;
+            int barHeight = (int) ((double) item.count() / maxCount * panelHeight * 0.9); // Scale to 80% of panel height
+            int x = widthOfLeftAxis + i * barWidth;
+            int y = heightOfBottomAxis + panelHeight - barHeight;
             g.fillRect(x, y, barWidth, barHeight); // Draw the bar
             g.setColor(Color.BLACK);
             g.drawString(String.valueOf(item.days()), x + barWidth / 4, panelHeight - 10); //Label underneath
+        }
+        var divisionHeight = panelHeight * 0.9 / 10;
+        var division = maxCount / 10;
+        for (int i = 0; i <= 10; i++) {
+            var y = heightOfBottomAxis + panelHeight - (int) (i * divisionHeight);
+            g.setColor(Color.DARK_GRAY);
+            g.drawLine(0, y, panelWidth, y);
+            g.setColor(Color.BLACK);
+            g.drawString("%4d".formatted(i * division), 5, y);
         }
     }
 }
