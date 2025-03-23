4564c9c819ffe4aba5810625000d559fa59b4b7f
Claudio Nieder <claudio.nieder@inodes.ch>
2025-03-23 20:34:08 +0100

    Added output so one sees in which phase the bot is.

--- src/main/java/ro/sellfluence/app/EmagBot.java
+++ src/main/java/ro/sellfluence/app/EmagBot.java
@@ -8,12 +8,15 @@ import java.sql.SQLException;
  */
 public class EmagBot {
     public static void main(String[] args) {
+        System.out.println("Syncing the eMag mirror database.");
         EmagDBApp.main(new String[0]);
         try {
+            System.out.println("Transfer orders from the database to the date comenzi sheet.");
             PopulateDateComenziFromDB.main(new String[0]);
         } catch (IOException | SQLException e) {
             throw new RuntimeException("PopulateDateComenzi endet with an exception ", e);
         }
+        System.out.println("Update the sheet used for customer feedback.");
         UpdateAllSheetsMain.main(new String[0]);
     }
 }
