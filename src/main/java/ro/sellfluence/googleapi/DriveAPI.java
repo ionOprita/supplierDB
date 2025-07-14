643bde44dc2646ec4cbd6c411d084f9a9f935e46
Claudio Nieder <claudio.nieder@inodes.ch>
2025-07-14 23:14:15 +0200

    If more than one file has the same name but one of them is owned by me then accept that one and do not throw an exception.

--- src/main/java/ro/sellfluence/googleapi/DriveAPI.java
+++ src/main/java/ro/sellfluence/googleapi/DriveAPI.java
@@ -7,6 +7,7 @@ import com.google.api.client.json.gson.GsonFactory;
 import com.google.api.services.drive.Drive;
 import com.google.api.services.drive.model.File;
 import com.google.api.services.drive.model.FileList;
+import ro.sellfluence.support.Logs;
 
 import java.io.IOException;
 import java.security.GeneralSecurityException;
@@ -14,10 +15,14 @@ import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
 import java.util.Objects;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import static ro.sellfluence.googleapi.Credentials.getCredentials;
 
 public class DriveAPI {
+    private static final Logger warnLogger = Logs.getConsoleLogger("DriveAPI", Level.WARNING);
+
     private static final JsonFactory jsonFactory = GsonFactory.getDefaultInstance();
     private static final Map<String, DriveAPI> nameToAPI = new HashMap<>();
 
@@ -26,7 +31,7 @@ public class DriveAPI {
     private final Map<String, String> nameForId = new HashMap<>();
 
     /**
-     * Initialize the drive API.
+     * Initialise the drive API.
      *
      * @param appName name of the application
      *                as registered in the <a href="https://console.cloud.google.com/apis/credentials/consent">console</a>
@@ -54,25 +59,36 @@ public class DriveAPI {
     public String getFileId(String name) {
         Objects.requireNonNull(name);
         try {
-            var matchingFiles = new HashSet<String>();
+            var matchingFiles = new HashSet<File>();
             String pageToken = null;
             do {
-                FileList fileList = setupDriveService().files().list().setQ("name='%s'".formatted(name)).setPageToken(pageToken).execute();
+                FileList fileList = setupDriveService().files().list().setQ("name='%s'".formatted(name)).setFields("*").setPageToken(pageToken).execute();
                 pageToken = fileList.getNextPageToken();
                 var files = fileList.getFiles();
                 files.stream()
                         .filter(f -> name.equals(f.getName()))
-                        .map(File::getId).forEach(matchingFiles::add);
+                        .forEach(matchingFiles::add);
 
             } while (pageToken != null);
             if (matchingFiles.isEmpty()) {
                 return null;
             } else if (matchingFiles.size() == 1) {
-                String fileId = matchingFiles.iterator().next();
+                String fileId = matchingFiles.iterator().next().getId();
                 nameForId.put(fileId, name);
                 return fileId;
             } else {
-                throw new RuntimeException("More than one file with matches %s.".formatted(name));
+                var myFiles = matchingFiles.stream().filter(File::getOwnedByMe).toList();
+                if (myFiles.size() == 1) {
+                    var fileId = myFiles.getFirst().getId();
+                    warnLogger.log(Level.WARNING, "Found more than one file with the name %s, using the single one owned by me with the ID %s.".formatted(name, fileId));
+                    nameForId.put(fileId, name);
+                    return fileId;
+                } else {
+                    throw new RuntimeException(
+                            "%d files with matches %s, %d files are owned by me."
+                                    .formatted(matchingFiles.size(), name, myFiles.size())
+                    );
+                }
             }
         } catch (IOException e) {
             throw new RuntimeException("Couldn't retrieve the list of files.", e);
@@ -80,7 +96,7 @@ public class DriveAPI {
     }
 
     /**
-     * Return the name for a file ID. This works only, if the file was already searched for
+     * Return the name for a file ID. This works only if the file was already searched for
      * using this API.
      *
      * @param id to look for
