198a7c49a7c39c3b8c3c2660c3d185cd469809a8
Claudio Nieder <claudio.nieder@inodes.ch>
2025-09-09 13:11:52 +0200

    Remove everything related to the old Gloablsources scraping idea that didn't work out. The branch "last-with-globalsources-code" was created so that it can be found in case someone wants to resurrect it.

--- src/main/java/ro/koppel/supplierDB/SupplierRepository.java
+++
@@ -1,9 +0,0 @@
-package ro.koppel.supplierDB;
-
-import org.springframework.data.repository.CrudRepository;
-public interface SupplierRepository extends CrudRepository<Supplier, Long> {
-
-    Supplier findByName(String name);
-
-    Supplier findByLink(String link);
-}
