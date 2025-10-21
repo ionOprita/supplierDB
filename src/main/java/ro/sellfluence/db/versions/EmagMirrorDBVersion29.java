ec8a1fd3baac5bd7b5c7ed5fe1aeed573ddc9ece
Claudio Nieder <claudio.nieder@inodes.ch>
2025-10-21 13:34:38 +0200

    Finish rebase.

--- src/main/java/ro/sellfluence/db/versions/EmagMirrorDBVersion29.java
+++ src/main/java/ro/sellfluence/db/versions/EmagMirrorDBVersion29.java
@@ -5,17 +5,19 @@ import java.sql.SQLException;
 
 import static ro.sellfluence.db.versions.EmagMirrorDBVersion1.executeStatement;
 
-class EmagMirrorDBVersion27 {
+class EmagMirrorDBVersion29 {
     /**
-     * Cache the tab within the employee sheet.
+     * Record last time of successful runs plus count of unsuccessful runs.
      *
      * @param db database connection to use.
      * @throws SQLException all errors are passed back to the caller.
      */
-    static void version27(Connection db) throws SQLException {
+    static void version29(Connection db) throws SQLException {
         executeStatement(db, """
-                ALTER TABLE product
-                  ADD COLUMN vendor UUID;
+                ALTER TABLE tasks ADD COLUMN last_successful_run TIMESTAMP;
+                """);
+        executeStatement(db, """
+                ALTER TABLE tasks ADD COLUMN unsuccessful_runs INTEGER;
                 """);
     }
 }
